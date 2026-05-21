f2db798127f15454cfaae074bbcd610005190948
colonel-karna <colonelkarna@gmail.com>
2026-05-21 23:46:15 +0545

    hhhhhh

--- demo1.py
+++ demo1.py
@@ -1,9 +1,11 @@
 import math
 print("The exact value of e is:",math.e)
 print("The value of pi is:",math.pi)
+karna  = [1,2,3,4,5]
 print("hello brother i just  added this line")
 def this():
     print("kaldfjaljdflajdlfa")
 print("hahaaaaa")
 
     
+print("The exact value of e is:",math.e)
