faf9ead7d5c64c3348089d9e513e19a4535d07ce
colonel-karna <colonelkarna@gmail.com>
2026-05-21 23:15:31 +0545

    .....

--- demo1.py
+++ demo1.py
@@ -4,7 +4,6 @@ print("The value of pi is:",math.pi)
 print("hello brother i just  added this line")
 def this():
     print("kaldfjaljdflajdlfa")
-print("hah colonel i hahaaaaaaaaaaaaaaaaaaaaaaaaa")
-print("balla talla vayo k vayo vayo")
+print("hahaaaaa")
 
     
