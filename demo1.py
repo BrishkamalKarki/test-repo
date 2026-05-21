60a5ad54fea916fe1c053114cdf4cc44ad643125
BrishkamalKarki <brishkamalkarki@gmail.com>
2026-05-21 23:55:48 +0545

    jjjjjjjjjjjjjjj

--- demo1.py
+++ demo1.py
@@ -1,5 +1,5 @@
 import math
-print("The exact value of e is:",math.e)
+print("The exact value of e is:", 3.14)
 print("The value of pi is:",math.pi)
 print("hello brother i just  added this line")
 def this():
