2d27b2b0051423893cd41eaa89be4fb6d4492db8
BrishkamalKarki <brishkamalkarki@gmail.com>
2026-05-22 06:44:06 +0545

    pikachu user thunderbolt

--- demo1.py
+++ demo1.py
@@ -3,7 +3,8 @@ print("The exact value of e is:", 3.14)
 print("The value of pi is:",math.pi)
 print("hello brother i just  added this line")
 def this():
-    print("kaldfjaljdflajdlfa")
-print("hahaaaaa")
+    print("kajdlfa")
+    print("ksdkfdk")
+print("hahaaaadsadfaa")
 
     
